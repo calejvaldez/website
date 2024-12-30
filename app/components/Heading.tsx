8083f7216afe06bde51ccdaf3b2534379b47859e
Carlos Valdez <cvaldezh@uci.edu>
2024-12-29 16:07:27 -0800

    feat(Heading.tsx): Link button appears only when hovered

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import styles from "./Heading.module.css"
 import { useRouter } from "next/navigation";
 
@@ -12,17 +12,30 @@ type ExportProps = {
 }
 
 export default function Heading({sectionId, level, children, hideLinkButton}: ExportProps) {
+    const [hovered, setHovered] = useState(false);
     const router = useRouter();
-    return <div id={sectionId} className={styles.container_wrapper}>
+
+    useEffect(() => {
+        if (hideLinkButton) {
+            setHovered(!hideLinkButton);
+        }
+    }, [hovered])
+
+    return <div
+        id={sectionId}
+        onMouseOver={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        className={styles.container_wrapper}
+    >
         
         {level === "h1" && <h1 style={{fontSize: '35px'}}>{children}</h1>}
         {level === "h2" && <h2 style={{fontSize: '30px'}}>{children}</h2>}
 
-        {!hideLinkButton && <Image onClick={() => {
+        <Image hidden={!hovered} onClick={() => {
             let path = `${window.origin}#${sectionId}`;
             navigator.clipboard.writeText(path);
             router.replace(path);
             
-        }} alt={`Copy section link for ${sectionId}`} src="./link-icon.svg" className={styles.link_button} width="20" height="20" />}
+        }} alt={`Copy link for ${sectionId}`} src="./link-icon.svg" className={styles.link_button} width="20" height="20" />
     </div>
 }
