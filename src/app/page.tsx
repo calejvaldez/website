4682a0b3a4a3b00114edf16ad9c593a2bb27687a
Carlos Valdez <cvaldezh@uci.edu>
2025-05-29 13:28:37 -0700

    fix: replace links to latest GH release

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
       </BigSection>
       <BigSection title="Projects" description="I work on a lot of projects. These are the ones I'm most proud of.">
         <div className={styles.projectsContainer}>
-          <ProjectButton name="Iris" role="Lead Developer" description="An open-source hospice management system. Tracks medications, video records, and more. Designed with NextJS and Tauri." link="https://ojosproject.org/iris/" imageSrc="https://ojosproject.org/images/iris-v0-summary.png" imageAlt="Various screenshots of the Iris software." startLabel="October 2023" endLabel="Present" />
-          <ProjectButton name="You Are Typing" role="Developer" description="A journaling app designed as a desktop chat app. Meant to help you become your own best friend. Designed with Vite and Tauri." link="https://github.com/calejvaldez/YouAreTyping/" imageSrc="https://raw.githubusercontent.com/calejvaldez/YouAreTyping/refs/heads/main/.github/assets/header.png" imageAlt="You Are Typing logo" startLabel="May 2023" endLabel="Present" />
+          <ProjectButton name="Iris" role="Lead Developer" description="An open-source hospice management system. Tracks medications, video records, and more. Designed with NextJS and Tauri." link="https://github.com/ojosproject/iris/releases/latest/" imageSrc="https://ojosproject.org/images/iris-v0-summary.png" imageAlt="Various screenshots of the Iris software." startLabel="October 2023" endLabel="Present" />
+          <ProjectButton name="You Are Typing" role="Developer" description="A journaling app designed as a desktop chat app. Meant to help you become your own best friend. Designed with Vite and Tauri." link="https://github.com/calejvaldez/YouAreTyping/releases/latest/" imageSrc="https://raw.githubusercontent.com/calejvaldez/YouAreTyping/refs/heads/main/.github/assets/header.png" imageAlt="You Are Typing logo" startLabel="May 2023" endLabel="Present" />
         </div>
       </BigSection>
     </>
