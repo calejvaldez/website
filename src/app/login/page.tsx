1ba4ae6caa4be3e4d5593496d36ae05e411ddc22
Carlos Valdez <self@calejvaldez.com>
2025-11-18 20:45:13 -0800

    feat: add authentication

---
+++ src/app/login/page.tsx
@@ -0,0 +1,30 @@
+import { signIn } from "@/auth";
+import styles from "./page.module.css";
+import { Discord } from "iconoir-react";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const redirectTo = (await searchParams).redirect?.toString();
+
+  return (
+    <section className={styles.container}>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("discord", { redirectTo });
+        }}
+        className={styles.form}
+      >
+        <h2>Sign in</h2>
+        <p>Hey! I need you to login to see that.</p>
+        <button type="submit" className="primary">
+          <Discord />
+          Sign in with Discord
+        </button>
+      </form>
+    </section>
+  );
+}
