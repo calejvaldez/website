f7df364e1fd70ee740f64d54b0157421f5a3be62
Carlos Valdez <self@calejvaldez.com>
2025-12-11 01:01:37 -0800

    chore: redirect to vldz.tech

--- prisma/migrations/20251117034949_add_index/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `unlisted` on the `PortfolioProjects` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "PortfolioProjects" DROP COLUMN "unlisted",
-ADD COLUMN     "index" INTEGER;
