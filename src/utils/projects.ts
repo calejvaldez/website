85a8160020f1fe40dbd35589e0d33cbc9cc06214
Carlos Valdez <carlos@vldz.tech>
2026-04-17 14:32:13 -0700

    feat: remove unnecessary stuff

--- src/utils/projects.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "./prisma";
-
-export async function getProjects() {
-  const projects = await prisma.project.findMany();
-  return projects
-    .filter((p) => p.isPublic && p.publicIndex !== null && p.promoteUrl)
-    .sort((a, b) => a.publicIndex! - b.publicIndex!);
-}
